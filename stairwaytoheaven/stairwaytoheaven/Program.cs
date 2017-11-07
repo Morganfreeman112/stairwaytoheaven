6f88c63551f24296c8eca13a795a3101540f8361
Morganfreeman112 <33232551+Morganfreeman112@users.noreply.github.com>
2017-11-07 14:20:02 -0500

    Finished StairwayToHeaven challenge m8

---
+++ stairwaytoheaven/stairwaytoheaven/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace stairwaytoheaven
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hey hows it goin you wanna make a staircase ah i dont care  ill make one no matter what put in a number will ya m8");
+			int Num = int.Parse(Console.ReadLine());
+			for (int i = 0; i < Num; i++)
+				Console.WriteLine(new String('*', i + 1).PadRight(Num, ' '));
+		}
+	}
+}
+
