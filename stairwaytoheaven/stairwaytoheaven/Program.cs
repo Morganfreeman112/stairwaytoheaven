c5ced7ba30c5cbf8e6df5babeab47dd32913e0d3
Morganfreeman112 <33232551+Morganfreeman112@users.noreply.github.com>
2017-11-23 14:04:27 -0500

    new changes

--- stairwaytoheaven/stairwaytoheaven/Program.cs
+++ stairwaytoheaven/stairwaytoheaven/Program.cs
@@ -6,10 +6,17 @@ namespace stairwaytoheaven
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hey hows it goin you wanna make a staircase ah i dont care  ill make one no matter what put in a number will ya m8");
-			int Num = int.Parse(Console.ReadLine());
-			for (int i = 0; i < Num; i++)
-				Console.WriteLine(new String('*', i + 1).PadRight(Num, ' '));
+			Console.WriteLine ("hey hows it goin wanna make a staircase why? because i make staircases put in a number my dude.");
+			int num = int.Parse(Console.ReadLine());
+				string msg = "";
+			int i = 0;
+			while (i < num)
+			{
+				if (i < num)
+					i++;
+					msg += "*";
+				    Console.WriteLine (msg);
+			}
 		}
 	}
 }
